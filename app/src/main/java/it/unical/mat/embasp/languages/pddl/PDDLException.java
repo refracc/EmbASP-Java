94c1cc51cc73bf8b6d27584a418a0b0b106d022d
refracc <40345422@live.napier.ac.uk>
2023-07-21 14:36:21 +0100

    remove hanging javadoc comment

--- app/src/main/java/it/unical/mat/embasp/languages/pddl/PDDLException.java
+++ app/src/main/java/it/unical/mat/embasp/languages/pddl/PDDLException.java
@@ -1,10 +1,10 @@
 package it.unical.mat.embasp.languages.pddl;
 
+import java.io.Serial;
+
 public class PDDLException extends Exception {
 
-  /**
-   *
-   */
+  @Serial
   private static final long serialVersionUID = 1L;
 
   public PDDLException(final String error) {
