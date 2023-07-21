ab530531bf4fbdaaa37b7e9861284699e46e8d21
refracc <40345422@live.napier.ac.uk>
2023-07-21 14:37:57 +0100

    Remove hanging javadoc comment

--- app/src/main/java/it/unical/mat/embasp/languages/ObjectNotValidException.java
+++ app/src/main/java/it/unical/mat/embasp/languages/ObjectNotValidException.java
@@ -1,10 +1,10 @@
 package it.unical.mat.embasp.languages;
 
+import java.io.Serial;
+
 public class ObjectNotValidException extends Exception {
 
-  /**
-   *
-   */
+  @Serial
   private static final long serialVersionUID = 1L;
 
   public ObjectNotValidException() {
