04317ad33d21398965b3706b85f811251840c0e5
refracc <40345422@live.napier.ac.uk>
2023-07-21 14:34:22 +0100

    Edut IllegalTermException

--- app/src/main/java/it/unical/mat/embasp/languages/asp/IllegalTermException.java
+++ app/src/main/java/it/unical/mat/embasp/languages/asp/IllegalTermException.java
@@ -1,10 +1,10 @@
 package it.unical.mat.embasp.languages.asp;
 
+import java.io.Serial;
+
 public class IllegalTermException extends Exception {
 
-  /**
-   *
-   */
+  @Serial
   private static final long serialVersionUID = 1L;
 
   public IllegalTermException(final String msg) {
